c24ef3dee80ba5d93a48ee585c9e4779ff5ddf95
rafal19987 <rafal987.1@o2.pl>
2023-02-17 11:09:24 +0100

    Vite config with React

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Root = () => {
+  return <div>Hello world</div>;
+};
+
+export default Root;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from './Pages/Root/Root';
+import './Sass/root.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Root />);
