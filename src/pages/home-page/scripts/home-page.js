df0b5bea74dc7107409d36279c1137336e91819d
rafal19987 <rafal987.1@o2.pl>
2023-02-17 10:07:44 +0100

    Fix: change import path for home page styles

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,7 +1,7 @@
 /// Homepage JavaScript File
 /// Here we import all the JavaScript files we need for our homepage.
 
-import '../home-page.scss'
+import '../styles/home-page.scss'
 
 import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
 
